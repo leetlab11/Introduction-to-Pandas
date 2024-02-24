1fee585e6745b13e3c580ae325a56d940bf620d7
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-02-23 21:52:42 -0600

    Update 01-02877-create-a-dataframe-from-list.py

--- 01-02877-create-a-dataframe-from-list.py
+++ 01-02877-create-a-dataframe-from-list.py
@@ -5,6 +5,7 @@
 #   [3, 11],
 #   [4, 20]
 # ]
+# columns parameter has name of columns in square brackets
 
 import pandas as pd
 
@@ -13,15 +14,10 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     df = pd.DataFrame(data = student_data, columns = ['student_id', 'age'])
     return df
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-# popular- same as above but concise
-# first parameter in DataFrame() is always data, not need to mention as above
-# columns parameter has name of columns in square brackets
-
-import pandas as pd
-
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-
+#    OR
+#    more popular
+#    first parameter in DataFrame() is always data, not need to mention as above
+    
     return(pd.DataFrame(student_data, columns = ['student_id', 'age']))
 
 
