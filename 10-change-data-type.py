02ccf94abe60cdc3753accc18af9494f81fe5449
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-03-02 15:04:57 -0500

    Create 10-change-data-type.py

---
+++ 10-change-data-type.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    
+    students['grade'] = students['grade'].astype(int)
+    return students
+
+
+# no companies listed
