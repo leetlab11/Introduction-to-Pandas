377149b85c93b82fa3a0927ee05e490d4015bd7e
leetlab11 <150235922+leetlab11@users.noreply.github.com>
2024-03-02 13:35:20 -0500

    Create 05-02881-create-a-new-column.py

---
+++ 05-02881-create-a-new-column.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    
+    employees['bonus'] = employees['salary'] * 2
+    return employees
+
+  
+# no companies listed
